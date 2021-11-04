d7980ada3837424e28de10ae0945435c3e27117a
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2021-11-05 00:29:07 +0300

    Add project files.

---
+++ BookList/Models/KLDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Models
+{
+    public class KLDbContext : DbContext
+    {
+        public KLDbContext(DbContextOptions<KLDbContext> options)  : base (options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
